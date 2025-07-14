285426734dc32fe92ea0996eac0284b96a78db34
Gabriel <gsilva.developer@gmail.com>
2025-07-14 19:47:31 -0400

    att

---
+++ oi/ConversaoStrings.java
@@ -0,0 +1,13 @@
+package oi;
+
+public class ConversaoStrings {
+    public static void main(String[] args) {
+        Integer a = 10000;
+        System.out.println(a.toString().length());
+
+        int b = 1000;
+        System.out.println(Integer.toString(b));
+        
+        System.out.println("" + a);
+    }
+}
--- oi/Wrappers.java
+++ oi/Wrappers.java
@@ -2,9 +2,13 @@ package oi;
 
 public class Wrappers {
     public static void main(String[] args) {
-        Byte b = 100;
-        Short s = 1000;
-        Integer i = 10000;
-        Long l = 100000L;
+
+        /* São Classes que envolvem os tipos primitivos
+         * eles são identificados com a letra Maiuscula 
+         */
+        // Byte b = 100;
+        // Short s = 1000;
+        // Integer i = 10000;
+        // Long l = 100000L;
     }
 }
