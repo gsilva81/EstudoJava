9e02b3bc015460689773194e5973203e25519be2
Gabriel <gsilva.developer@gmail.com>
2025-07-14 20:59:08 -0400

    novo

---
+++ oi/ConversaoNumero.java
@@ -0,0 +1,14 @@
+package oi;
+
+import javax.swing.JOptionPane;
+
+public class ConversaoNumero{
+    public static void main(String[] args) {
+
+        //JOptionPane.showInputDialog abre uma janela 
+
+        String a = JOptionPane.showInputDialog("digite um numero: ");
+        String b = JOptionPane.showInputDialog("Digite outro numero");
+        
+    }
+}
