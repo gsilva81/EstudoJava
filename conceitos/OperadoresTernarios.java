956a4f25cb6bbe79405b7ce690ced71ca2881ccd
gsilva81 <gsilva.developer@gmail.com>
2025-08-15 17:33:07 -0400

    att

---
+++ conceitos/OperadoresTernarios.java
@@ -0,0 +1,10 @@
+package conceitos;
+
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+        double a1 = 8.5;
+        String result = a1 >= 7.0 ? "Aprovado" : "Recuperação";
+
+        System.out.println("O aluno foi " + result);
+    }
+}
